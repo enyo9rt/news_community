1adcb304f4e8961739d578c7e0288cb58772329f
최찬혁 <cksgurwkd12@naver.com>
2022-05-07 22:31:58 +0900

    FEAT: 테스트를 위한 가짜 로그인 기능 구현(JWT return)

--- application.py
+++ application.py
@@ -4,6 +4,8 @@ from dev_module import news_getter
 from dev_module import weather
 from DB_ADMIN import account
 from dev_module.comments import comments
+from datetime import datetime, timedelta
+import jwt
 
 application = Flask(__name__)
 # weather.py 파일로 날씨 관련 api 분리 후 가져오기
@@ -12,7 +14,7 @@ application.register_blueprint(comments)
 
 client = MongoClient(account.API_KEY)
 db = client.Haromony
-
+SECRET_KEY = 'test'
 
 @application.route('/')
 def home():
@@ -28,6 +30,25 @@ def news_get():
     """
     news_list = news_getter.get_news()
     return jsonify({'news_list': news_list})
+
+
+@application.route('/fake_signin', methods=['POST'])
+def fake_sign_in():
+    # 로그인
+    username_receive = 'test_id'
+    result = db.users.find_one({'username': username_receive})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').encode().decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '똥'})
 
 
 if __name__ == '__main__':
