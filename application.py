7f958f7245e7320977d001815b75a1e9ad75759d
YoungeunJo <youngeun.jo1494@gmail.com>
2022-05-09 20:44:57 +0900

    FEAT : 로그인, 회원가입 기능 추가

--- application.py
+++ application.py
@@ -6,6 +6,7 @@ from DB_ADMIN import account
 from dev_module.comments import comments
 from datetime import datetime, timedelta
 import jwt
+import hashlib
 
 application = Flask(__name__)
 # weather.py 파일로 날씨 관련 api 분리 후 가져오기
@@ -41,6 +42,7 @@ def news_get():
     return jsonify({'news_list': news_list})
 
 
+
 @application.route('/fake_signin', methods=['POST'])
 def fake_sign_in():
     # 로그인
@@ -72,6 +74,57 @@ def profile(userid):
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("/"))
 
+@application.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+
+@application.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'user_id': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@application.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "user_id": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "nick_name": username_receive,  # 프로필 이름 기본값은 아이디
+        "profile_pic": "",  # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png",  # 프로필 사진 기본 이미지
+        "profile_info": ""  # 프로필 한 마디
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@application.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"user_id": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
 if __name__ == '__main__':
     application.run('0.0.0.0', port=5000, debug=True)
