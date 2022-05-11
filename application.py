cf35e91b7ae7db0b8a4e8e1b46f6825b5da8642e
최찬혁 <cksgurwkd12@naver.com>
2022-05-12 00:19:58 +0900

    Refactor: application.py 내의 불필요한 코드 제거

--- application.py
+++ application.py
@@ -1,8 +1,7 @@
 from site_view import main_site, join_site, detail_site, profile_site
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-from pymongo import MongoClient
+from flask import Flask
 from dev_module import weather
-from CONFIG import account
+
 
 
 application = Flask(__name__)
@@ -11,11 +10,6 @@ application.register_blueprint(join_site.join_page)
 application.register_blueprint(detail_site.detail_page)
 application.register_blueprint(weather.weather_api)
 application.register_blueprint(profile_site.profile_page)
-
-client = MongoClient(account.API_KEY)
-db = client.Haromony
-
-SECRET_KEY = 'test'
 
 if __name__ == '__main__':
     application.run('0.0.0.0', port=5000, debug=True)
