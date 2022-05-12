4fcdadb6695f2bb5c81e6b36af32b13ae6d9c211
최찬혁 <cksgurwkd12@naver.com>
2022-05-12 10:01:19 +0900

    FEAT: XSS 구현을 위해 Sanitizer import

--- dev_module/xss_protect.py
+++ dev_module/xss_protect.py
@@ -1,6 +1,11 @@
+from html_sanitizer import Sanitizer
+
 
 class xxs_protect():
     @staticmethod
     def stop_code_filter(target):
-        target = target.replace('<', '&lt;').replace('>', '&gt;').replace('\r\n', '<br>')
+        sanitizer = Sanitizer()
+        # target = target.replace('<', '&lt;').replace('>', '&gt;').replace('\r\n', '<br>')
+        target = sanitizer.sanitize(target)
+
         return target
