0af18f03250d502774c1dc7fbf9aab635b9baa2b
Yeah-PC\blueflow <blueflow109@gmail.com>
2022-05-12 10:55:14 +0900

    Docs: js파일 재분류

--- static/js/detail.js
+++ static/js/detail.js
@@ -240,39 +240,3 @@ function toggle_bookmark(post_id) {
 
     }
 }
-
-// 북마크 기사 가져오기
-function posts_get(user_id) {
-    $("#comment-box").empty()
-    $.ajax({
-        type: "GET",
-        url: `/posts_get?user_id_give=${user_id}`,
-        data: {},
-        success: function (response) {
-            console.log(response["msg"])
-            if (response["result"] == "success") {
-                let posts = response["posts"]
-                for (let i = 0; i < posts.length; i++) {
-                        let post = posts[i]
-                        let temp_html = `<div class="bookmark_post box" id="${post["post_id"]}">
-                                            <a href="/detail/${post["post_id"]}">${post["title"]}</a>
-                                        </div>`
-                        $("#comment-box").append(temp_html)
-                    }
-            }
-        }
-    })
-}
-
-// 프로필 탭
-function toggle_tab(type) {
-    console.log(type)
-    let $li_tab = $(`#${type}`)
-    if (`${type}`=="posts") {
-        $li_tab.addClass("is-active").siblings().removeClass("is-active")
-        posts_get(now_user_id)
-    } else if (`${type}`=="comments") {
-        $li_tab.addClass("is-active").siblings().removeClass("is-active")
-        comments_get(now_user_id,"")
-    }
-}
